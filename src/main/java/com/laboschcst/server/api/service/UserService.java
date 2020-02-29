64165046949959eabdad2e58b90ad37b9055d983
Dzsani <gatsjanos@gmail.com>
2020-02-29 17:40:56 +0100

    Added dockerfile healthcheck.

--- src/main/java/com/laboschcst/server/api/service/UserService.java
+++ src/main/java/com/laboschcst/server/api/service/UserService.java
@@ -4,9 +4,6 @@ import com.laboschcst.server.api.dto.ProfileDetailsDto;
 import com.laboschcst.server.entity.ProfileDetails;
 import com.laboschcst.server.exceptions.ContentNotFoundApiException;
 import com.laboschcst.server.repo.Repos;
-import org.codehaus.jackson.node.JsonNodeFactory;
-import org.codehaus.jackson.node.ObjectNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
