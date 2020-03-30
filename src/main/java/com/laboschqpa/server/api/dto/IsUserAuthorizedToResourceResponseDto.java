211fd8362c8d14076b9c1c0fae8b249d6686571d
Dzsani <gatsjanos@gmail.com>
2020-03-30 06:25:07 +0200

    Fixing tests.

--- src/main/java/com/laboschqpa/server/api/dto/IsUserAuthorizedToResourceResponseDto.java
+++ src/main/java/com/laboschqpa/server/api/dto/IsUserAuthorizedToResourceResponseDto.java
@@ -1,5 +1,6 @@
 package com.laboschqpa.server.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class IsUserAuthorizedToResourceResponseDto{
     @Builder.Default
     private boolean authorized = false;
