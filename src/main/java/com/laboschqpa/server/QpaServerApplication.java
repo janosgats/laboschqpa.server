b22d079e51679edc92e75b2a2008446fb0b447fe
Dzsani <gatsjanos@gmail.com>
2020-04-02 01:31:45 +0200

    Commit to test docker build cache.

--- src/main/java/com/laboschqpa/server/QpaServerApplication.java
+++ src/main/java/com/laboschqpa/server/QpaServerApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QpaServerApplication
-{
+public class QpaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(QpaServerApplication.class, args);
     }
