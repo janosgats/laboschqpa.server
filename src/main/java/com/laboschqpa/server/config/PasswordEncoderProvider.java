2c30723b97afe41acddd86430aa2bc91009cace1
Dzsani <gatsjanos@gmail.com>
2020-04-26 23:02:49 +0200

    Using ARP instead of NIO.

--- src/main/java/com/laboschqpa/server/config/PasswordEncoderProvider.java
+++ src/main/java/com/laboschqpa/server/config/PasswordEncoderProvider.java
@@ -2,6 +2,7 @@ package com.laboschqpa.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,6 +11,6 @@ public class PasswordEncoderProvider {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
