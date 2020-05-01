eda3255f7310f7fed0409ed06ed03d64e3d54a8e
Dzsani <gatsjanos@gmail.com>
2020-05-01 15:10:27 +0200

    Swagger ignore AuthenticationPrincipal.

--- src/main/java/com/laboschqpa/server/config/Swagger2Config.java
+++ src/main/java/com/laboschqpa/server/config/Swagger2Config.java
@@ -3,6 +3,7 @@ package com.laboschqpa.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,7 +17,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/**"))
                 .build().apiInfo(apiEndPointsInfo());
