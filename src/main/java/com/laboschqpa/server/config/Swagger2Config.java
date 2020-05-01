31b739023c77976eb22490cd86c5506bb184e3e7
Dzsani <gatsjanos@gmail.com>
2020-05-01 15:21:47 +0200

    Swagger added csrf token header.

--- src/main/java/com/laboschqpa/server/config/Swagger2Config.java
+++ src/main/java/com/laboschqpa/server/config/Swagger2Config.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +23,15 @@ public class Swagger2Config {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .globalOperationParameters(
+                        Arrays.asList(new ParameterBuilder()
+                                .name("X-CSRF-TOKEN")
+                                .description("CSRF token")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build())
+                )
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/**"))
