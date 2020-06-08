082239d38f0f9492fb20a27424603894a5c6fb5a
Dzsani <gatsjanos@gmail.com>
2020-06-08 16:40:49 +0200

    k8s_dev CORS trial.

---
+++ src/main/java/com/laboschqpa/server/config/K8sDevCorsConfig.java
@@ -0,0 +1,15 @@
+package com.laboschqpa.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("k8s_dev")
+@Configuration
+public class K8sDevCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("**").allowedMethods("*");
+    }
+}
