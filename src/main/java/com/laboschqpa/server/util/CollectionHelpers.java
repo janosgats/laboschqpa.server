408cebdbfe7b008f21b3216f0f6c5cfc39fdb8a9
Dzsani <gatsjanos@gmail.com>
2021-09-24 13:10:53 +0200

    Fixed attachment removal initial capacity

--- src/main/java/com/laboschqpa/server/util/CollectionHelpers.java
+++ src/main/java/com/laboschqpa/server/util/CollectionHelpers.java
@@ -8,7 +8,7 @@ public class CollectionHelpers {
     public static <T> HashSet<T> subtractToSet(Collection<T> whole, Collection<T> toSubtract) {
         final HashSet<T> toSubtractSet = new HashSet<>(toSubtract);
 
-        final HashSet<T> result = new HashSet<>(whole.size() - toSubtract.size());
+        final HashSet<T> result = new HashSet<>(Math.min(whole.size() - toSubtract.size(), 0));
         for (T itemFromWhole : whole) {
             if (!toSubtractSet.contains(itemFromWhole)) {
                 result.add(itemFromWhole);
